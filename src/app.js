6232589248910c7e12319e7c4e6ec0ed9d172fa9
huy-hoang.phan <huyhoangprojects@gmail.com>
2018-03-23 00:45:03 +0700

    export default

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // import './utils.js';
 
-import {square, add} from './utils.js';
+import minus, {square, add} from './utils.js';
 console.log('app.js is not running!');
 console.log(square(4));
 console.log(add(10, 4));
+console.log(minus(10, 6));
